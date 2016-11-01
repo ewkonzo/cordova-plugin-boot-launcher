8cf7de615bc7d88c8b31ebfc4f5b7d9f77a2314b
cmseely <cmseely@umich.edu>
2016-11-01 13:36:03 -0400

    Update BootLauncher.java

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -10,6 +10,7 @@ public class BootLauncher extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Intent serviceIntent = new Intent(context, com.homedepot.timeclock.MainActivity.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        System.out.println('launcher context ' + context);
         context.startActivity(serviceIntent);
     }
 }
