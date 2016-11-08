66848029aa334e3262c537b86940d016a4793222
Eliya Wolfram Konzo <ewkonzo@gmail.com>
2016-11-08 09:39:27 +0300

    Update BootLauncher.java
    hue hue

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -8,7 +8,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, com.homedepot.timeclock.MainActivity.class);
+        Intent serviceIntent = new Intent(context, com.eliyakonzo.risiti.MainActivity.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         System.out.println('launcher context ' + context);
         context.startActivity(serviceIntent);
